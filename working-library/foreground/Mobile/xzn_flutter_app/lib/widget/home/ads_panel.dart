89d2e6e485c2a99a68a0dcd7b54b647331887908
sshhhll <sshhhll@sina.com>
2020-06-22 13:27:55 +0800

    点击广告图片进入商品详情

--- working-library/foreground/Mobile/xzn_flutter_app/lib/widget/home/ads_panel.dart
+++ working-library/foreground/Mobile/xzn_flutter_app/lib/widget/home/ads_panel.dart
@@ -2,40 +2,52 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:xzn/conf/config.dart';
 import 'package:xzn/models/ads.dart';
+import 'package:xzn/models/product.dart';
+import 'package:xzn/page/product/product_show.dart';
 import 'package:xzn/page/web_view.dart';
 import 'package:xzn/services/ads_service.dart';
+import 'package:xzn/services/product_service.dart';
+import 'package:xzn/services/token.dart';
 
 class AdsCard extends StatelessWidget {
   AdsCard({this.ads, this.mini: false});
   bool mini;
   Ads ads;
+  Product _product;
+  String token;
 
   @override
   Widget build(BuildContext context) {
     Widget placeholder = Image.asset(
-        "assets/image/default_picture.webp", //头像占位图，加载过程中显示
-        fit: BoxFit.cover,
-      );
-    String imageUrl = Config.baseUrl()+"user/adpic/"+ads.picture;
-    return Expanded(
-      child: GestureDetector(
-        child: CachedNetworkImage(
-          height: mini?60:80,
-          imageUrl: imageUrl,
-          fit: BoxFit.cover,
-          placeholder: (context, url) => placeholder,
-          errorWidget: (context, url, error) => placeholder,
-        ),
-        onTap: () {
-          if (ads.type == "url")
-            Navigator.push(context,
-              MaterialPageRoute(builder: (context) {
-                return WebView(url: ads.data);
-              }));
-        },
-      )
-//      Image.network(Config.baseUrl()+"user/adpic/"+ads.picture)
+      "assets/image/default_picture.webp", //头像占位图，加载过程中显示
+      fit: BoxFit.cover,
     );
+    String imageUrl = Config.baseUrl() + "user/adpic/" + ads.picture;
+    return Expanded(
+        child: GestureDetector(
+      child: CachedNetworkImage(
+        height: mini ? 60 : 80,
+        imageUrl: imageUrl,
+        fit: BoxFit.cover,
+        placeholder: (context, url) => placeholder,
+        errorWidget: (context, url, error) => placeholder,
+      ),
+      onTap: () async {
+        if (ads.type == "url") {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return WebView(url: ads.data);
+          }));
+        } else {
+          _product =
+              await getProductDetails(getToken(context), ads.data.toString());
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return ProductPage(product: _product);
+          }));
+        }
+      },
+    )
+//      Image.network(Config.baseUrl()+"user/adpic/"+ads.picture)
+        );
   }
 }
 
@@ -69,41 +81,54 @@ class _AdsPanelState extends State<AdsPanel> {
             );
           } else {
             print(snapshot.data[0].toJson());
-              widget = Column(
-                children: <Widget>[
+            widget = Column(
+              children: <Widget>[
 //                  ...snapshot.data.map((cartItem) {
 //                    return Text("C");
 //                  }),
-                  Flex(
-                    direction: Axis.horizontal,
-                    children: <Widget>[
-                      AdsCard(ads: snapshot.data[0],),
-                      AdsCard(ads: snapshot.data[1],)
-                    ],
-                  ),
+                Flex(
+                  direction: Axis.horizontal,
+                  children: <Widget>[
+                    AdsCard(
+                      ads: snapshot.data[0],
+                    ),
+                    AdsCard(
+                      ads: snapshot.data[1],
+                    )
+                  ],
+                ),
 //                  SizedBox(
 //                    height: 30,
 //                  ),
-                  Flex(
-                    direction: Axis.horizontal,
-                    children: <Widget>[
-                      AdsCard(ads: snapshot.data[2], mini: true,),
-                      AdsCard(ads: snapshot.data[3], mini: true,),
-                      AdsCard(ads: snapshot.data[4], mini: true,)
-                    ],
-                  ),
-                ],
-              );
+                Flex(
+                  direction: Axis.horizontal,
+                  children: <Widget>[
+                    AdsCard(
+                      ads: snapshot.data[2],
+                      mini: true,
+                    ),
+                    AdsCard(
+                      ads: snapshot.data[3],
+                      mini: true,
+                    ),
+                    AdsCard(
+                      ads: snapshot.data[4],
+                      mini: true,
+                    )
+                  ],
+                ),
+              ],
+            );
           }
         } else {
           widget = Container(
-            alignment: Alignment.center,
-            width: double.infinity,
-            height: 200,
-            child: Padding(
-              padding: EdgeInsets.all(20),
-              child: CircularProgressIndicator(),
-            ));
+              alignment: Alignment.center,
+              width: double.infinity,
+              height: 200,
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: CircularProgressIndicator(),
+              ));
         }
         return widget;
       },
