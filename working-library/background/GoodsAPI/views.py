ac0061c1d24e99b18d03b6f6e439f47ddd97d782
Johnny <1394909965@qq.com>
2020-06-22 15:16:50 +0800

    Revert "goods广告"
    This reverts commit ff7e6d9f9bed118c8cf56ba51bb8726986205219.

--- working-library/background/GoodsAPI/views.py
+++ working-library/background/GoodsAPI/views.py
@@ -7,28 +7,38 @@ from django_redis import get_redis_connection
 from LoginAPI.token_module import get_token,out_token
 @csrf_exempt
 def Goodsdetail(request):
-    #token = request.POST.get("token")
+    token = request.POST.get("token")
     gid = request.POST.get("product_id")
-    goods = Goods.objects.filter(goods_id=gid).values()[0]
-    product_name = goods['goods_name']
-    product_id = gid
-    price = {"num": goods['price'], "unit": str(goods['unit'])}
-    shuffle,detail = getpicture(gid)
-    print(shuffle,detail)
-    picture_list = {"shuffle": shuffle, "detail": detail}
-    details = {"origin": goods['origin'], "specification": goods['specification'],
-               "packaging": goods['packaging'], "stockway": goods['stockway'], "weight": goods['weight']}
-    stock = goods['stock']
-    discount = goods['discount']
-    description = {"subtitle": goods['subtitle'], "distribution": goods['distribution'], "promotion": goods['promotion']}
-    tags = {"type": goods['tags_type'], "content": goods['tags_content']}
-    data = {"product_name": str(product_name), "product_id": str(product_id), "price": price,
-            "picture_list": picture_list, "details": details, "stock": stock, "discount": discount,
-            "description": description, "tags": tags}
-    response = json.dumps(data)
-    print(response)
-    return HttpResponse(response)
+    user = User.objects.filter(token=token)
+    if user:
+        user=user.values()[0]
+        telephone = user['phone']
+        if out_token(telephone, token):
+            goods = Goods.objects.filter(goods_id=gid).values()[0]
 
+            product_name = goods['goods_name']
+            product_id = gid
+            price = {"num": goods['price'], "unit": str(goods['unit'])}
+            shuffle,detail = getpicture(gid)
+            print(shuffle,detail)
+            picture_list = {"shuffle": shuffle, "detail": detail}
+            details = {"origin": goods['origin'], "specification": goods['specification'],
+                       "packaging": goods['packaging'], "stockway": goods['stockway'], "weight": goods['weight']}
+            stock = goods['stock']
+            discount = goods['discount']
+            description = {"subtitle": goods['subtitle'], "distribution": goods['distribution'], "promotion": goods['promotion']}
+            tags = {"type": goods['tags_type'], "content": goods['tags_content']}
+
+            data = {"product_name": str(product_name), "product_id": str(product_id), "price": price,
+                    "picture_list": picture_list, "details": details, "stock": stock, "discount": discount,
+                    "description": description, "tags": tags}
+            response = json.dumps(data)
+            print(response)
+            return HttpResponse(response)
+    data = {"message": "Failed"}
+    response = json.dumps(data)
+    #print(response)
+    return HttpResponse(response)
 
 @csrf_exempt
 def getpicture(id):
